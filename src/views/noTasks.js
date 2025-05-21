ef2192a322910c70356cc328959ddadb8efb6714
Derek Antrican <derekantrican@gmail.com>
2025-05-20 22:48:42 -0700

    Revert styling

--- src/views/noTasks.js
+++ src/views/noTasks.js
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export function NoTasksView() {
   return (
-    <div style={{}}>
+    <div style={{marginTop: 'auto', marginBottom: 'auto'}}>
       {/*Todo: adjust styling*/}
       <Typography>
         No Tasks were found (if you were expecting tasks, you might want to check the filter in settings)
